33cc4f32cd8bd802debd1f024395f595eec765b8
Juan Lara <julara@unal.edu.co>
2023-05-28 18:53:33 -0500

    Added path searcher

---
+++ src/asyncmake/search/base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from re import Pattern
+from typing import Self
+from pydantic import BaseModel
+
+class SearchConfig(BaseModel):
+    start_path: Path
+    pattern: Pattern
+    max_depth: int
+
+class AbstractSearcher(ABC):
+    config: SearchConfig
+    path: Path
+
+    def setup(self: Self, search_config: SearchConfig) -> Self:
+        self.config = search_config
+        return self
+    
+    @abstractmethod
+    def search(self: Self) -> Self:
+        ...
---
+++ src/asyncmake/search/searcher.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from re import Match
+from typing import Callable, Self
+from asyncmake.search.base import AbstractSearcher
+
+class Searcher(AbstractSearcher):
+    def search(self: Self) -> Self:
+        fn_match: Callable[[Path], Match | None] = lambda path: re.search(self.config.pattern, path.stem)
+        cur_depth = 1
+        cur_path = self.config.start_path
+        files = map(fn_match, cur_path.glob("*"))
+        matches = filter(lambda x: x is not None, files)
+        while not matches and cur_depth != self.config.max_depth:
+            cur_path = cur_path / ".."
+            cur_depth += 1
+            files = map(fn_match, cur_path.glob("*"))
+            matches = filter(lambda x: x is not None, files)
+        if matches:
+            self.path = cur_path
+
+        return self
