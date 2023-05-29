fe0d2af6f9fb67a3d1af7cca6e43f44e7a2e6735
Juan Lara <julara@unal.edu.co>
2023-05-28 23:41:08 -0500

    Telescope integration

--- lua/asyncmake/init.lua
+++ lua/asyncmake/init.lua
@@ -1,4 +1,68 @@
-local function make_command(args)
-    vim.cmd("MakeFn")
+local notify = require("notify")
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+local function make_command(opts)
+    local function remote_call()
+        cmd = {"MakeFn", opts.args}
+        cmd = table.concat(cmd, " ")
+        notify("Start: "..cmd)
+        vim.api.nvim_exec2(cmd, {})
+        notify("End: "..cmd)
+    end
+
+    async.run(remote_call)
 end
-vim.api.nvim_create_user_command("Make", make_command)
+
+local function make_start_msg(opts)
+    cmd = {"make", opts.args}
+    cmd = table.concat(cmd, " ")
+    notify("Start: "..cmd)
+end
+
+local function make_end_msg(opts)
+    cmd = {"make", opts.args}
+    cmd = table.concat(cmd, " ")
+    notify("End: "..cmd)
+end
+
+local function make_err(opts)
+    cmd = {"make", opts.args}
+    cmd = table.concat(cmd, " ")
+    notify("Error: "..cmd, "error")
+end
+
+vim.api.nvim_create_user_command("MakeStartMsg", make_start_msg, {nargs='?'})
+vim.api.nvim_create_user_command("MakeEndMsg", make_end_msg, {nargs='?'})
+vim.api.nvim_create_user_command("MakeErr", make_err, {nargs='?'})
+
+local function split(text, sep)
+    local t={}
+    for str in string.gmatch(text, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
+local make_fuzzy = function(opts)
+    values = vim.api.nvim_exec2("MakeParsedOpts", {output=true})
+    pickers.new(opts, {
+        prompt_title = "make",
+        finder = finders.new_table {
+            results = split(values.output, " ")
+        },
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                vim.api.nvim_exec2("Make "..selection[1], {})
+            end)
+            return true
+        end,
+    }):find()
+end
+
+vim.api.nvim_create_user_command("MakeFuzzy", make_fuzzy, {nargs='?'})
--- rplugin/python3/plugin.py
+++ rplugin/python3/plugin.py
@@ -1,11 +1,11 @@
 import re
 from pathlib import Path
-from typing import List
-from pynvim import plugin, command
+from typing import Callable, List
+from pynvim import plugin, command, function
 from pynvim.api import Nvim
 from asyncmake.search.searcher import Searcher
 from asyncmake.search.base import SearchConfig
-from asyncmake.parse.base import ParseConfig
+from asyncmake.parse.base import ParseConfig, Rule
 from asyncmake.parse.parser import parse, load
 from asyncmake.exec.base import Command
 from asyncmake.exec.make import MakeExecutor
@@ -30,12 +30,28 @@ class AsyncMake:
                 max_depth=5
             )
         self.parse_config = ParseConfig(
-                rule_pattern=re.compile(r"^(?P<rule>\w+):")
+                rule_pattern=re.compile(r"^(?P<rule>[a-zA-Z\-_%]+):")
                 )
 
-    @command(name="MakeFn", nargs="*", sync=True, range="") # type: ignore
+    @command(name="Make", nargs="*", sync=False, range="") # type: ignore
     def make(self, args: Args, range=None):
         cmd = " ".join(args)
+        self.nvim.api.exec2(f"MakeStartMsg {cmd}", {})
         self.search_config.start_path = Path(str(self.nvim.call("getcwd")))
         self.searcher.search()
+            
         self.executor.setup(command=Command(cmd=cmd), path=self.searcher.path).execute()
+        if self.executor.command.status == 0:
+            self.nvim.api.exec2(f"MakeEndMsg {cmd}", {})
+        else:
+            self.nvim.api.exec2(f"MakeErr {cmd} with status {self.executor.command.status}", {})
+
+    @command("MakeParsedOpts", nargs=0, sync=True, range="")
+    def make_parsed_opts(self, args: Args, range=None) -> str:
+        fn_rule: Callable[[Rule], str] = lambda rule: rule.name
+        self.search_config.start_path = Path(str(self.nvim.call("getcwd")))
+        self.searcher.search()
+        file = load(self.searcher.path / "Makefile")
+        parsed_file = parse(file, self.parse_config)
+        rules = map(fn_rule, parsed_file.rules)
+        self.nvim.out_write(" ".join(rules) + "\n")
