b54a91a9933f03c29de2c533fa5492d9c88fabbb
Juan Lara <julara@unal.edu.co>
2023-05-28 20:51:43 -0500

    Basic make command

--- test/test_exec.py
+++ test/test_exec.py
@@ -1,14 +1,14 @@
-import os
+from pathlib import Path
 from asyncmake.exec.base import Command
 from asyncmake.exec.make import MakeExecutor
 
 
 class TestExecutor:
     def test_make(self):
-        os.chdir("test/data/")
+        path = Path("test/data")
         command = Command(cmd="rule1")
-        executor = MakeExecutor().setup(command=command).execute()
+        executor = MakeExecutor().setup(command=command, path=path).execute()
         assert executor.command.status == 0
         command = Command(cmd="rule_2")
-        executor.setup(command=command).execute()
+        executor.setup(command=command, path=path).execute()
         assert executor.command.status == 0
